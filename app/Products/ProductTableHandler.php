ad12225d82d8f150c8810880b4b9c5b10fca9987
myanmaronlinepos <myanmaronlinepos@gmail.com>
2019-07-07 00:17:01 +0630

    edit producttablehandler

--- app/Products/ProductTableHandler.php
+++ app/Products/ProductTableHandler.php
@@ -10,7 +10,8 @@ class ProductTableHandler
  public function getProducts($user_id)
  {
 
-  return Product::select('product_name')->where('user_id', $user_id)->get();
+  return Product::select('product_id','product_name','category_id','tag_id','price_sell')
+                    ->where('user_id', $user_id)->get();
  }
 
  public function getProduct($product_id)
